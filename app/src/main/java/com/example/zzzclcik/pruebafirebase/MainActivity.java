5fdd69c216002557f059860102eeca07d36c2e44
Zzzclick <oso.osiel@gmail.com>
2017-02-05 22:40:39 -0600

    SE esta trabajando en ello

--- app/src/main/java/com/example/zzzclcik/pruebafirebase/MainActivity.java
+++ app/src/main/java/com/example/zzzclcik/pruebafirebase/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.zzzclcik.pruebafirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 import com.google.firebase.iid.FirebaseInstanceId;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mensajeTextView;
+    private EditText mensajeEditText;
 
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mensajeRef = ref.child("mensaje");
 
     public static final String TAG = "NOTICIAS";
 
@@ -19,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         infoTextView = (TextView) findViewById(R.id.infoTextView);
+        mensajeTextView = (TextView) findViewById(R.id.textViewBase);
+        mensajeEditText = (EditText) findViewById(R.id.editTextBase);
+        Button boton1 = (Button)findViewById(R.id.buttonBase);
 
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
@@ -30,5 +45,36 @@ public class MainActivity extends AppCompatActivity {
         String token = FirebaseInstanceId.getInstance().getToken();
 
         Log.d(TAG, "Token: " + token);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+modificar();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mensajeRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                mensajeTextView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void modificar() {
+        String mensaje = mensajeEditText.getText().toString();
+        mensajeRef.setValue(mensaje);
+        mensajeEditText.setText("");
+    }
+
 }
